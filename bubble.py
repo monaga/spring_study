32e55001838cff2571614ef648e6c0e2f469e927
kaoruko tomonaga <cielmonaga@gmail.com>
2017-02-17 14:36:57 +0900

    fixed some lines

--- bubble.py
+++ bubble.py
@@ -1,10 +1,10 @@
 x=[9, 6, 7, 1, 2]
 for i in range(1, len(x)):
-    j=i
-    while (j > 0) and (x[j-1]>x[j]):
+    j = i
+    while (j > 0) and (x[j - 1] > x[j]):
         tmp = x[j]
-        x[j] = x[j-1]
-        x[j-1] = tmp
-        j=j-1
+        x[j] = x[j - 1]
+        x[j - 1] = tmp
+        j = j - 1
         # print(x)
 print(x)
