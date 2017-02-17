a7bde0e2796241e7d428a590ee5852e14dbd6d9b
kaoruko tomonaga <cielmonaga@gmail.com>
2017-02-17 13:18:16 +0900

    fix some lines

--- nabeatsu.py
+++ nabeatsu.py
@@ -1,5 +1,7 @@
-for i in range(1,101):
-    if (i %3 ==0 or i % 10 == 3):
+for i in range(1, 101):
+    if i % 3 == 0 or "3" in str(i):
         print("aho!")
+    # elif str(i).find("3") == 1 :
+    #     print("aho!")
     else:
         print(i)
