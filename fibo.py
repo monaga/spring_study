8492e030c35c653d3dca9fa623aeaf5efaaba03b
kaoruko tomonaga <cielmonaga@gmail.com>
2017-02-24 14:26:14 +0900

    fixed some lines

--- fibo.py
+++ fibo.py
@@ -1,8 +1,8 @@
-com = input('入力 :')
-print('出力：', com)
-a = int(com)
+fibo_num = input('入力 :')
+print('出力：', fibo_num)
+a = int(fibo_num)
 x = [0, 1]
 for i in range(a - 2):
      com = x[i] + x[i + 1]
-     x.append(com)
+     x.append(fibo_num)
 print(x)
